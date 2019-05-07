aac1316bc91db2b0f4cdaa7e2f132d35e4e89b1d
Japnix <brendan.tawa@gmail.com>
2019-05-07 00:55:26 -0400

    Added stock

--- zerobot.py
+++ zerobot.py
@@ -1,9 +1,12 @@
 import discord
 from discord.ext import commands
-import random
+import urllib.request
+import json
+import datetime
 import sys
 
-mytoken = sys.argv[1]
+discordtoken = sys.argv[1]
+stocktoken = sys.argv[2]
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
@@ -19,19 +22,41 @@ async def on_ready():
     print('------')
 
 @bot.command()
-async def announcement(ctx, *, announcement):
+async def announce(ctx, *, announcement):
 
    channel = None
 
    for x in ctx.guild._channels:
-       if bot.get_channel(x).name == 'announcement':
+       if bot.get_channel(x).name == 'announcement' or bot.get_channel(x).name == 'announcements':
            channel = bot.get_channel(x)
 
    if channel is None:
        await ctx.channel.send('There is no #announcement channel in this guild')
+   else:
+       await channel.send(ctx.author.display_name + ': ' + announcement)
 
 
-   await channel.send(ctx.author.mention + ': ' + announcement)
+@bot.command()
+async def stock(ctx, *, query):
+    request_url = 'https://cloud.iexapis.com/stable/tops?token=' + stocktoken + '&symbols=' + query
+    embed = discord.Embed(title='Stock Queries',
+                          timestamp=datetime.datetime.utcnow())
 
+    try:
+        data = urllib.request.urlopen(request_url)
+        content = data.read().decode('utf-8')
+        data = json.loads(content)
 
-bot.run(mytoken)
+        if len(data) == 0:
+            await ctx.channel.send(embed=discord.Embed(title="No Stock Matches"))
+        elif len(data) == 1:
+            embed.add_field(name=data[0]['symbol'], value=data[0]['lastSalePrice'])
+            await ctx.channel.send(embed=embed)
+        else:
+            for tickers in data:
+                embed.add_field(name=tickers['symbol'], value=tickers['lastSalePrice'])
+            await ctx.channel.send(embed=embed)
+    except:
+        return
+
+bot.run(discordtoken)
